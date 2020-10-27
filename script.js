72d64e9c99e6068b78755f765779bf3bb4b3d8dd
Mohit Bajaj <mohit_b35@hotmail.com>
2020-10-27 17:02:40 +0530

    Functionality Step 1 - fetch + show search results 1. Call unsplash API with the keyword 'travel' to fetch results 2. Iterate through results and convert each result into a grid item

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menuButton = document.getElementById('btn-menu');
 const navMenu = document.getElementById('nav-menu');
+const resultsGrid = document.getElementById('results-grid');
 
 menuButton.addEventListener('click', toggleMenu);
 
@@ -7,3 +8,50 @@ function toggleMenu(){
 	navMenu.classList.toggle('open');
 	this.classList.toggle('open');
 }
+
+async function searchImages(searchTerm, pageSize = 10, currentPage = 1) {
+	const searchURL = 
+		`${BASE_URL}/search/photos?query=${searchTerm}
+		&page=${currentPage}
+		&per_page=${pageSize}
+		&order_by=latest`;
+	// Get image results
+	const response = await fetch(searchURL,{
+		method: 'GET',
+		headers: {
+			'Accept-Version': 'v1',
+			'Authorization': `Client-ID ${UCK}`
+		}
+	});
+
+	let data = await response.json();
+
+	renderGrid(data.results);
+}
+
+// Convert image results from API to HTML and populate grid
+function renderGrid(imageList) {
+	const gridHTML = imageList.map(imageDetails => renderCard(imageDetails)).join('');
+
+	resultsGrid.innerHTML = gridHTML;
+}
+
+// Generate HTML for a single card
+function renderCard(imageDetails) {
+	return (
+		`
+		<article class="result-card">
+			<img class="result-image" src="${imageDetails.urls.small}">
+			<h2 class="result-title">${imageDetails["alt_description"]}</h2>
+		</article>
+		`
+	);
+}
+
+
+searchImages('travel');
+
+
+// Get image results
+// Construct image grid
+// Load HTML
